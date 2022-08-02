6b00a2b3d8b3f6e5006bf9a29b798933e96f5f45
Simon Franco <simonfranco0@gmail.com>
2022-08-01 19:05:13 -0700

    update

--- mecurySedan.js
+++ mecurySedan.js
@@ -7,6 +7,63 @@ const VehicleModule = require("./vehicleBaseClass")
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 class Car extends Vehicle{
-    constructor(make, model, year, color, mileage)
+    constructor(make, model, year, color, mileage){
     super (make, model, year, color, mileage)
+    this.maxPassengers = 5
+    this.passenger = 0
+    this.numberOfWheels = 4
+    this.maxSpeed = 160
+    this.fuel = 10
+    this.scheduleService = false
+}
+start() {
+    if (this.fuel > 0) {            
+        console.log("engine has started.");
+        return this.started = true
+    } else {
+        console.log("no fuel");
+        return this.started = false;
+    }
+}
+accelerate() {
+    if (this.started) {
+        if (this.fuel > 0) {
+            console.log(this.speed += 1);
+            this.fuel = this.fuel - 1;
+        } else {
+            console.log("out of fuel.");
+            this.stop();
+        }
+    } else {
+        alert("You need to start the engine first.");
+    }
+}
+decelerate() {
+    if (this.started) {
+        if (this.fuel > 0) {
+            if (this.speed > 0) {
+                console.log(this.speed -= 1);
+                this.fuel = this.fuel - 1;
+            } else {
+                console.log(this + " has stopped moving");
+                this.fuel = this.fuel - 1;
+            }
+        } else {
+            console.log("out of fuel.");
+            this.stop();
+        }
+    } else {
+        alert("You need to start the engine first.");
+    }
+}
+stop() {
+    this.started = false;
+}
+
+drive() {
+    accelerate();
+}
+brake() {
+    decelerate();
+}
 }
